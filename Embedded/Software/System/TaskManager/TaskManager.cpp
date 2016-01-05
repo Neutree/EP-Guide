5b5a04a2e47817a900ef8e29d4adac62500a1067
Neutree <czd666666@gmail.com>
2016-01-05 23:18:15 +0800

    ### update taskmanager

--- Embedded/Software/System/TaskManager/TaskManager.cpp
+++ Embedded/Software/System/TaskManager/TaskManager.cpp
@@ -2,24 +2,29 @@
 
 TaskManager tskmgr;
 
-volatile u32 TaskManager::_it_cnt = 0;
+double TaskManager::_it_time = 0;
+double TaskManager::_new_time = 0;
+double TaskManager::_old_time = 0;
+
 //Constructor
 TaskManager::TaskManager()
 {
 	SysTick->CTRL &= 0xFFFFFFFB; //Clock div 8 = 9M
-	SysTick->LOAD = 9000000;     //1s
+	SysTick->LOAD = 16200000;     //1.8s
 	SysTick->CTRL |= 0x00000003; //INT +ENABLE
 }	
 //get current time since power on
 double TaskManager::Time(void)
 {
-	static double newT = 0, oldT = 0;
-	newT = _it_cnt + 1.0 - double(SysTick->VAL) / 9000000.0;
-	if(newT-oldT>0.99) newT -= 1;
-	oldT = newT;
-	return newT;
+	_new_time = _it_time + 1.8 - SysTick->VAL/9000000.0; //update current time
+	
+	if(_new_time - _old_time > 1.799) //check if breaked by SysTick interrupt
+	{	
+		_new_time -= 1.8;	              //calibrate current time
+	}		          
+	_old_time = _new_time;            //update old time
+	return _new_time;
 }
-
 //微秒延时
 void TaskManager::DelayUs(u16 nus)
 {
@@ -45,6 +50,8 @@ extern "C"
 {
 	void SysTick_Handler(void)
 	{
-		TaskManager::_it_cnt++;
+		TaskManager::_it_time += 1.8;
 	}
 }
+
+
--- Embedded/Software/System/TaskManager/TaskManager.h
+++ Embedded/Software/System/TaskManager/TaskManager.h
@@ -1,23 +1,28 @@
 #ifndef _TASK_MANAGER_H_
 #define _TASK_MANAGER_H_
-
-extern "C"{
 #include "stm32f10x.h"
-void SysTick_Handler(void);
-}
 
 class TaskManager
 {
+private:
+	static double _new_time;		 //current updated time
+	static double _old_time;     //last updated time
 public:
-	volatile static u32 _it_cnt;      //SysTick interrupt counter
+	static double _it_time;      //time = SysTick interrupt counter*1.8s
 public:		
-	TaskManager();
-	static double Time(void);
+	TaskManager();               //constructor
+	static double Time(void);    //get current time
 	static void DelayUs(u16 nus);
 	static void DelayMs(u16 nms);
 	static void DelayS(u16 s);
 };
-
 extern TaskManager tskmgr;
+
+#define	MOD_ERROR  0x00
+#define	MOD_READY  0x01
+#define	MOD_BUSY   0x02
+#define	MOD_LOCK   0x04
+#define	MOD_UNLOCK 0x08
+
 
 #endif
